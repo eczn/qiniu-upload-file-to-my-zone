0f4ddd13bbb083d168da2d0c742b247808e6dc49
eczn <1975845422@qq.com>
2018-04-18 23:22:23 +0800

    [+] get path

--- src/up2zone-up.js
+++ src/up2zone-up.js
@@ -1,9 +1,49 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-    , { qnx, wait } = require('eczn-utils')
     , pkg_config = require('../package.json')
-// Init 
+    , path = require('path')
+    , { qnx, wait } = require('eczn-utils')
+    , configer = require('./utils/configer')
+    , config = configer.get()
+    , globby = require('globby')
+; 
+
+// init 
+let { AK, SK, DOMAIN, BUCKET } = config; 
+qnx.init(AK, SK, DOMAIN, BUCKET); 
 
 
-qnx.init('AK', 'SK', 'domain', 'bucket')
+function getPaths(location, remote){
+    location = location.replace(/\\/g, '/');
+    let location_all = path.join(location, '**/*').replace(/\\/g, '/');
+
+    const locations_paths = await globby(location_all);
+    const relative_paths = locations_paths.map(
+        e => {
+            let temp = e.replace(location, ''); 
+
+            if (temp[0] !== '/'){
+                return '/' + temp; 
+            } else {
+                return temp; 
+            }
+        }
+    ); 
+
+    console.log(location)
+    console.log(relative_paths); 
+}
+
+program
+    .version(pkg_config.version)
+    .arguments('[location] [remote]')
+    .action(async (location, remote) => {
+        getPaths(location, remote); 
+        
+    })
+    .parse(process.argv)
+
+
+
+
